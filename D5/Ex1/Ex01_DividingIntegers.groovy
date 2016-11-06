4ad79a0a846b2b1ff7e947f29507f84f0855b227
essvee <svince04@mail.bbk.ac.uk>
2016-11-06 21:50:26 +0000

    tidying dir structure

---
+++ D5/Ex1/Ex01_DividingIntegers.groovy
@@ -0,0 +1,16 @@
+Calculator myCalculator = new Calculator()
+
+int addResult = myCalculator.doAddition(11,5)
+println "Result returned for addition is: " + addResult
+
+int subResult = myCalculator.doSubtraction(15,4)
+println "Result returned for subtraction is: " + subResult
+
+int multiplyResult = myCalculator.doMultiplication(3,4)
+println "Result returned for multiplication is: " + multiplyResult
+
+double divisionResult = myCalculator.doDivision(9,2)
+println "Result returned for division is: " + divisionResult
+
+int moduloResult = myCalculator.doModulo(9,5)
+println "Result returned for modulo is: " + moduloResult
