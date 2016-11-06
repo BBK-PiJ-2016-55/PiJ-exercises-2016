d1b01840014a2c019a8a4540ac8ef0a4aa5dfe8b
essvee <svince04@mail.bbk.ac.uk>
2016-11-06 22:47:15 +0000

    first commit

---
+++ D5/Ex3/ArrayCopier.java
@@ -0,0 +1,22 @@
+public class ArrayCopier {
+
+	// Copies elements of src[] to dst[], truncating/padding when dst has fewer/more elements 
+	public int[] copy(int[] src, int[] dst) { 
+		if (src.length < dst.length){
+			for (int i = 0; i < src.length; i++) {
+				dst[i] = src[i];
+			} 
+			for (int i = src.length; i < dst.length; i++) {
+				dst[i] = 0;
+			}
+			return dst;
+		} else {
+			for (int i = 0; i < dst.length; i++) {
+				dst[i] = src[i];
+			}
+			return dst;
+		}
+	}
+
+
+}
---
+++ D5/Ex3/Launcher.java
@@ -0,0 +1,28 @@
+public class Launcher {
+    public static void main(String[] args){
+        int[] src = new int[6];
+        src[0] = 1;
+        src[1] = 2;
+        src[2] = 3;
+		src[3] = 4;
+        src[4] = 5;
+        src[5] = 6;
+
+		int[] dst = new int[5];
+        dst[0] = 1;
+        dst[1] = 2;
+        dst[2] = 3;
+		dst[3] = 4;
+		dst[4] = 5;
+		
+        ArrayCopier myCopier = new ArrayCopier();
+		dst = myCopier.copy(src, dst);
+		
+		System.out.println("New value of dst: ");
+		
+		for (int i = 0; i < dst.length; i++) {
+			System.out.println(dst[i]);
+		}
+		
+    }
+}
