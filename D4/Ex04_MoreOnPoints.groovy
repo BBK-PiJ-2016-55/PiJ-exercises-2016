6326e496a5c8e06b558661f6d33be5726e199885
essvee <svince04@mail.bbk.ac.uk>
2016-11-06 14:24:51 +0000

    added moveTo method

--- D4/Ex04_MoreOnPoints.groovy
+++ D4/Ex04_MoreOnPoints.groovy
@@ -17,14 +17,19 @@ double distanceToOrigin() {
 	return distance
 }
 
-/* // Changes to coordinates of this point to the given parameters
-double moveTo(double x, double y) {
+ // Changes the coordinates of this point to the given parameters
+void moveTo(double x, double y) {
+	this.x = x
+	this.y = y
 }
 
 // Changes the coordinates of this point to those of the given point
-double moveTo(Point){
+void moveTo(Point otherPoint){
+	this.x = otherPoint.x
+	this.y = otherPoint.y
 }
 
+/*
 // Returns a copy of the current point with the same coordinates`
 double clone(){
 }
@@ -36,16 +41,27 @@ double opposite(){
 }
 
 Point firstPoint = new Point();
-firstPoint.x =4;
+firstPoint.x = 4;
 firstPoint.y = 7;
 
 Point secondPoint = new Point();
 secondPoint.x = 7;
 secondPoint.y = -3;
 
-double distanceBetween = firstPoint.distanceTo(secondPoint)
-println "Distance between firstPoint and secondPoint is " + distanceBetween
+// double distanceBetween = firstPoint.distanceTo(secondPoint)
+// println "Distance between firstPoint and secondPoint is " + distanceBetween
 
-double distanceOrigin = firstPoint.distanceToOrigin()
-println "Distance between firstPoint and origin is " + distanceOrigin
+// double distanceOrigin = firstPoint.distanceToOrigin()
+// println "Distance between firstPoint and origin is " + distanceOrigin
 
+/* println "Enter new x coordinate for firstPoint: "
+String str = System.console().readLine()
+double firstX = Integer.parseInt(str)
+println "Enter new y coordinate for firstPoint: "
+str = System.console().readLine()
+double firstY = Integer.parseInt(str)
+firstPoint.moveTo(firstX, firstY) */
+
+firstPoint.moveTo(secondPoint)
+println "New value of x for firstPoint is " + firstPoint.x
+println "New value of y for firstPoint is " + firstPoint.y
