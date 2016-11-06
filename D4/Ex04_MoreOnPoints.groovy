331927eebd9eb251460f8d2624d3540cef61bd3c
essvee <svince04@mail.bbk.ac.uk>
2016-11-06 14:14:57 +0000

    added distanceTo and distanceToOrigin methods

--- D4/Ex04_MoreOnPoints.groovy
+++ D4/Ex04_MoreOnPoints.groovy
@@ -4,42 +4,48 @@ double y;
 
 // Calculates the distance to another point 
 double distanceTo(Point otherPoint) {
-	double distance = Math.sqrt(Math.pow((thisPoint.x - otherPoint.x),2) + Math.pow((thisPoint.y - otherPoint.y),2))
+	double distance = Math.sqrt(Math.pow((this.x - otherPoint.x),2) + Math.pow((this.y - otherPoint.y),2))
 	return distance
 }
 
 // Calculates distance to the origin
-distanceToOrigin() {
+double distanceToOrigin() {
+	Point originPoint = new Point()
+	originPoint.x = 0
+	originPoint.y = 0
+	double distance = distanceTo(originPoint)
+	return distance
 }
 
-// Changes to coordinates of this point to the given parameters
-moveTo(double x, double y) {
+/* // Changes to coordinates of this point to the given parameters
+double moveTo(double x, double y) {
 }
 
 // Changes the coordinates of this point to those of the given point
-moveTo(Point){
+double moveTo(Point){
 }
 
 // Returns a copy of the current point with the same coordinates`
-clone(){
+double clone(){
 }
 
 // Returns a copy of the current point with the cooridnates * by -1
-opposite(){
-}
+double opposite(){
+} */
 
 }
 
 Point firstPoint = new Point();
-firstPoint.x = 4.0;
-firstPoint.y = 1.0;
+firstPoint.x =4;
+firstPoint.y = 7;
 
 Point secondPoint = new Point();
-secondPoint.x = 5.0;
-secondPoint.y = 2.0;
+secondPoint.x = 7;
+secondPoint.y = -3;
 
 double distanceBetween = firstPoint.distanceTo(secondPoint)
-println "Distance between firstPoint and secondPOint is " + distanceBetween
+println "Distance between firstPoint and secondPoint is " + distanceBetween
 
-
+double distanceOrigin = firstPoint.distanceToOrigin()
+println "Distance between firstPoint and origin is " + distanceOrigin
 
