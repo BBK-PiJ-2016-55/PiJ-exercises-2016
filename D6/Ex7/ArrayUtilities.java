de591239861c05e76f6720d4c183545276b93dae
essvee <svince04@mail.bbk.ac.uk>
2016-11-28 17:22:41 +0000

    Added ex7

---
+++ D6/Ex7/ArrayUtilities.java
@@ -0,0 +1,22 @@
+/**
+* ArrayUtilities takes an array of integers and returns a linked list of integers in the same order as
+* the original array
+*/
+
+public class ArrayUtilities {
+	
+	public static ElementList toList(int[] myArray){
+		
+		ElementList myList = new ElementList();
+		
+		for (int i = 0; i < myArray.length; i++) {
+			Element myElement = new Element(myArray[i]);
+			myList.addElement(myElement);
+			myElement.printValue();
+			}
+		
+		return myList;
+		}
+		
+
+	}
