08fa396500a5d9b339236dcec96980df50ec9864
Sarah <svince04@mail.bbk.ac.uk>
2016-12-29 14:05:29 +0000

    Almost working - need to fix directions and looping

--- D5/Ex7/Target.java
+++ D5/Ex7/Target.java
@@ -1,24 +1,43 @@
 public class Target {
-	private int[][][] targetZone;
+	private int[][][] range;
+	private int targetX;
+	private int targetY;
+	private int targetZ;
 
-	// create a 3-D array of integers of the proposed size in 3D
+	// create a 3-D array of integers of the proposed size
 	// default value of each element is 0
 	public Target(int size){
-		this.targetZone = new int[size][size][size];
+		this.range = new int[size][size][size];
 	}
 
 	// set one random element in array to 1
 	public void init(){
-		int x = (int) Math.abs((targetZone.length) * Math.random());
-		int y = (int) Math.abs((targetZone.length) * Math.random());
-		int z = (int) Math.abs((targetZone.length) * Math.random());
+		targetX = (int) Math.abs((range.length) * Math.random());
+		targetY = (int) Math.abs((range.length) * Math.random());
+		targetZ = (int) Math.abs((range.length) * Math.random());
 		// Plane location (testing)
-		System.out.println(x + ", " + y + ", " + z);
-		targetZone[x][y][z] = 1;
+		System.out.println(targetX + ", " + targetY + ", " + targetZ);
+		range[targetX][targetY][targetZ] = 1;
 	}
 
 	public Result fire(int x, int y, int z){
-		return Result.HIT;
+		if (x > range.length || y > range.length || x > range.length) {
+			return Result.OUT_OF_RANGE;
+		} else if (range[x][y][z] == 1) {
+			return Result.HIT;
+		} else if (x < targetX) {
+			return Result.FAIL_LEFT;
+		} else if (x > targetX) {
+			return Result.FAIL_RIGHT;
+		} else if (y < targetY) {
+			return Result.FAIL_HIGH;
+		} else if (y > targetY) {
+			return Result.FAIL_LOW;
+		} else if (z > targetZ) {
+			return Result.FAIL_SHORT;
+		} else {
+			return Result.FAIL_LONG;
+		}
 	}
 
 }
