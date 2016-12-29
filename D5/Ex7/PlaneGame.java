b3b10525c681f345109e45786a53d041e9b9b6a3
Sarah <svince04@mail.bbk.ac.uk>
2016-12-29 18:01:56 +0000

    put in a loop to prompt replay

--- D5/Ex7/PlaneGame.java
+++ D5/Ex7/PlaneGame.java
@@ -5,52 +5,66 @@ public class PlaneGame {
 	}
 
 	public void launch() {
+		boolean playAgain = false;
+
+		do {
+		boolean finished = false;
+		playAgain = false;
 		Target WarIsHell = new Target(5);
 		WarIsHell.init();
-
 		System.out.println("Incoming! Try to bring the plane down!");
-		
-		System.out.println("Enter an X coordinate: ");
-		String str = System.console().readLine();
-		int x = Integer.parseInt(str);
-		System.out.println("Enter a Y coordinate: ");
-		str = System.console().readLine();
-		int y = Integer.parseInt(str);
-		System.out.println("Enter a Z coordinate: ");
-		str = System.console().readLine();
-		int z = Integer.parseInt(str);
-		Result myResult = WarIsHell.fire(x,y,z);
-		
-		switch (myResult) {
-			case OUT_OF_RANGE:
-				System.out.println("That shot is way out of range! Try harder!");
-				break;
-			case HIT:
-				System.out.println("A hit! Nice shot.");
-				break;
-			case FAIL_LEFT:
-				System.out.println("You missed! The target is to the left.");
-				break;
-			case FAIL_RIGHT:
-				System.out.println("You missed! The target is to the right.");
-				break;
-			case FAIL_HIGH:
-				System.out.println("You missed! The target is higher.");
-				break;
-			case FAIL_LOW:
-				System.out.println("You missed! The target is lower.");
-				break;
-			case FAIL_SHORT:
-				System.out.println("You missed! The target is nearer.");
-				break;
-			case FAIL_LONG:
-				System.out.println("You missed! The target is further away.");
-				break;
-			default:
-				System.out.println("Error - default case");
-				break;
+		// Prompt user for coordinates until they score a hit
+		while (!finished) {
+			System.out.println("Enter an X coordinate: ");
+			String str = System.console().readLine();
+			int x = Integer.parseInt(str);
+			System.out.println("Enter a Y coordinate: ");
+			str = System.console().readLine();
+			int y = Integer.parseInt(str);
+			System.out.println("Enter a Z coordinate: ");
+			str = System.console().readLine();
+			int z = Integer.parseInt(str);
+			Result myResult = WarIsHell.fire(x,y,z);
+			
+			// Respond with result and option to continue if successful
+			switch (myResult) {
+				case OUT_OF_RANGE:
+					System.out.println("That shot is way out of range! Try harder!");
+					break;
+				case HIT:
+					System.out.println("A hit! Nice shot!");
+					finished = true;
+					System.out.println("Would you like to play again?");
+					str = System.console().readLine();
+					if (str.equals("y") || str.equals("Y")) {
+						playAgain = true;							
+					} 
+					break;
+				case FAIL_LEFT:
+					System.out.println("You missed! The target is to the left.");
+					break;
+				case FAIL_RIGHT:
+					System.out.println("You missed! The target is to the right.");
+					break;
+				case FAIL_HIGH:
+					System.out.println("You missed! The target is higher.");
+					break;
+				case FAIL_LOW:
+					System.out.println("You missed! The target is lower.");
+					break;
+				case FAIL_SHORT:
+					System.out.println("You missed! The target is nearer.");
+					break;
+				case FAIL_LONG:
+					System.out.println("You missed! The target is further away.");
+					break;
+				default:
+					System.out.println("Error - default case");
+					break;
 
-		}
-
+				}
+			}
+		} while (playAgain);
 	}
+
 }
--- D5/Ex7/Target.java
+++ D5/Ex7/Target.java
@@ -15,29 +15,28 @@ public class Target {
 		targetX = (int) Math.abs((range.length) * Math.random());
 		targetY = (int) Math.abs((range.length) * Math.random());
 		targetZ = (int) Math.abs((range.length) * Math.random());
-		// Plane location (testing)
-		System.out.println(targetX + ", " + targetY + ", " + targetZ);
 		range[targetX][targetY][targetZ] = 1;
 	}
 
 	public Result fire(int x, int y, int z){
-		if (x > range.length || y > range.length || x > range.length) {
+		if (x >= range.length || y >= range.length || x >= range.length) {
 			return Result.OUT_OF_RANGE;
 		} else if (range[x][y][z] == 1) {
 			return Result.HIT;
-		} else if (x < targetX) {
-			return Result.FAIL_LEFT;
 		} else if (x > targetX) {
+			return Result.FAIL_LEFT;
+		} else if (x < targetX) {
 			return Result.FAIL_RIGHT;
-		} else if (y < targetY) {
-			return Result.FAIL_HIGH;
 		} else if (y > targetY) {
+			return Result.FAIL_HIGH;
+		} else if (y < targetY) {
 			return Result.FAIL_LOW;
 		} else if (z > targetZ) {
 			return Result.FAIL_SHORT;
 		} else {
 			return Result.FAIL_LONG;
 		}
+
 	}
 
 }
