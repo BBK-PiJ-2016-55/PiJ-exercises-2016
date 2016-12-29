3c8de46a0b73963c99c7bf63f6623c32b93ace8c
Sarah <svince04@mail.bbk.ac.uk>
2016-12-29 13:02:51 +0000

    Added game file and init()

---
+++ D5/Ex7/PlaneGame.java
@@ -0,0 +1,12 @@
+public class PlaneGame {
+	public static void main(String[] args){
+		PlaneGame game = new PlaneGame();
+		game.launch();
+	}
+
+	public void launch() {
+		Target WarIsHell = new Target(5);
+		WarIsHell.init();
+
+	}
+}
--- D5/Ex7/Target.java
+++ D5/Ex7/Target.java
@@ -1,14 +1,20 @@
 public class Target {
 	private int[][][] targetZone;
 
-	// creates a 3-D array of integers of the proposed size in 3D
+	// create a 3-D array of integers of the proposed size in 3D
+	// default value of each element is 0
 	public Target(int size){
 		this.targetZone = new int[size][size][size];
 	}
 
+	// set one random element in array to 1
 	public void init(){
-	// Sets all but one (random) element in matrix to 0
-	// One element set to 1
+		int x = (int) Math.abs((targetZone.length) * Math.random());
+		int y = (int) Math.abs((targetZone.length) * Math.random());
+		int z = (int) Math.abs((targetZone.length) * Math.random());
+		// Plane location (testing)
+		System.out.println(x + ", " + y + ", " + z);
+		targetZone[x][y][z] = 1;
 	}
 
 	public Result fire(int x, int y, int z){
