bd7d2233eaebd8aa36b09b9034a1d0fb3ee6e731
confd06 <svince04@mail.bbk.ac.uk>
2017-01-23 19:37:56 +0000

    first commit

---
+++ D14/Ex02/Launcher.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Launcher {
+	private static int userInt;
+	
+    public static void main(String[] args){
+		System.out.println("Enter a number: ");
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		userInt = Integer.parseInt(str);
+		Launcher launcher = new Launcher();
+		launcher.launch(userInt);
+	}
+
+	public void launch(int userInput) {
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		intList.add(4);
+		intList.add(5);
+		intList.add(6);
+		try {
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		// Will complain on compilation because NullPointerError was already caught 
+		} catch (NullPointerException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
