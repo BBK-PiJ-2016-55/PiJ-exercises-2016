94e1d49d348ebad61349a7cc1ea537ce9ccbc801
essvee <svince04@mail.bbk.ac.uk>
2017-02-20 20:08:22 +0000

    first commit

---
+++ D18/src/Ex03/Counter.java
@@ -0,0 +1,11 @@
+package Ex03;
+
+public class Counter {
+    private int n = 0;
+    public synchronized void increase() {
+        n++;
+    }
+    public int getCount() {
+        return n;
+    }
+}
